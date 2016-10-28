c4382d8701c956bd07e6156d0fb64f0611415c47
Dean12 <dean.j.magee@gmail.com>
2016-10-28 13:10:50 +1100

    fixed tooltip

--- static/js/maps.js
+++ static/js/maps.js
@@ -3,7 +3,7 @@ var mymap = L.map('mapid').setView([-36.5083988,145.0811384], 7);
 var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
 L.tileLayer(
-        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
             attribution: '&copy; ' + mapLink + ' Contributors',
             maxZoom: 15,
         }).addTo(mymap);
@@ -42,13 +42,15 @@ var colorScale = chroma
 
 //topoLayer.eachLayer(handleLayer);
 
+
+
 function handleLayer(layer){
-  var randomValue = Math.random(),
+  var randomValue = Math.random();
     fillColor = colorScale(randomValue).hex();
 
   layer.setStyle({
     fillColor : fillColor,
-    fillOpacity: 0.7,
+    fillOpacity: 0.6,
     color:'#555',
     weight:1,
     opacity:1
@@ -60,24 +62,46 @@ function handleLayer(layer){
   });
 }
 
-var $tooltip = $('.country-name');
 
-function enterLayer(){
-  var countryName = this.feature.properties.vic_lga__2;
-  $tooltip.text(countryName).show();
-  this.bringToFront();
-  this.setStyle({
+
+function enterLayer(e){
+  var layer = e.target
+  layer.bringToFront();
+  info.update(layer.feature.properties);
+  layer.setStyle({
     weight:3,
     opacity: 1
   });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
 }
 
 function leaveLayer(){
-  $tooltip.hide();
-
+  info.update();
   this.bringToBack();
   this.setStyle({
     weight:1,
     opacity:1
   });
 }
+
+// Popup tooltip that shows what LGA is under mouseover
+var info = L.control();
+
+info.onAdd = function (mymap) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    //console.log(props.vic_lga__2);
+    this._div.innerHTML = '<h4>Local Government Area</h4>' +  (props ?
+        '<b>' + props.vic_lga__2 + '</b>'
+        : 'Hover over a LGA');
+};
+
+info.addTo(mymap);
